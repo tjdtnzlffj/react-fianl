b72a1a3ae3dc2dc13c03552d457b60900b4f725e
SungSuu <tjdtnzlffj@gmail.com>
2021-08-20 10:50:42 +0900

    최종

--- src/components/breadCommunity/Community.js
+++ src/components/breadCommunity/Community.js
@@ -6,7 +6,6 @@ import { useDispatch, useSelector } from "react-redux";
 import queryString from "query-string";
 
 const Community = () => {
-<<<<<<< HEAD
   const history = useHistory();
   const dispatch = useDispatch();
   const commentDispatch = useDispatch();
@@ -51,65 +50,6 @@ const Community = () => {
       </div>
     </>
   );
-=======
-
-	const history = useHistory();
-	const dispatch = useDispatch();
-	const commentDispatch = useDispatch();
-	const boardList = useSelector(state => state.community);
-	const { search } = useLocation();
-
-	//queryString(ex - ?keyword=12345)에 찾고 싶은 키워드? 키? 가 없으면 undefined가 뜬다.
-	const { keyword } = queryString.parse(search);
-	useEffect(() => {
-		dispatch({ type: "FETCH_BOARDLIST" });
-		commentDispatch({ type: "FETCH_COMMENTLIST" });
-	}, []);
-
-	return (
-		<>
-			<div>
-				<Button
-					variant="outlined"
-					color="primary"
-					style={{
-						marginTop: "40px",
-						marginLeft: "31.5%",
-						marginBottom: "2px",
-						cursor: "pointer",
-					}}
-					onClick={() => {
-						history.push("/communityForm");
-					}}
-				>
-					글 작성
-				</Button>
-
-				{
-					keyword ?
-						boardList.filter(post => post.postTitle.includes(keyword)).map((board) => {
-
-							return (
-								<BreadItem
-									key={board.id}
-									board={board}
-								/>
-							);
-						})
-						:
-						boardList.map((board) => {
-
-							return (
-								<BreadItem
-									key={board.id}
-									board={board}
-								/>
-							);
-						})}
-			</div>
-		</>
-	);
->>>>>>> upstream/master
 };
 
 export default Community;
